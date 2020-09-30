31b17caca4c6274c8f70d1ceb639f837346ae0dc
kryptondv <kryptondevpl@gmail.com>
2020-09-30 16:00:25 +0200

    create routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
 const App = () => {
   return (
     <div>
-      App
+      <Router>
+        <Route path="/" exact component={StreamList} />
+        <Route path="/streams" exact component={StreamCreate} />
+        <Route path="/streams/edit" component={StreamEdit} />
+        <Route path="/streams/delete" component={StreamDelete} />
+        <Route path="/streams/show" component={StreamShow} />
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
