c3536c0120425536dc98d2e20b5cb83f96c7af2b
kryptondv <kryptondevpl@gmail.com>
2020-10-02 16:27:06 +0200

    add google auth logic

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,16 +1,57 @@
 import React, { Component } from 'react';
 
 export class GoogleAuth extends Component {
+  state = {
+    isSignedIn: null
+  }
+
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
       window.gapi.client.init({
         clientId: process.env.REACT_APP_GOOGLE_CLIENT_ID,
         scope: 'email',
+      }).then(()=>{
+        this.auth = window.gapi.auth2.getAuthInstance();
+        this.onAuthChange();
+        this.auth.isSignedIn.listen(this.onAuthChange);
       });
     });
   }
+
+  onAuthChange = () => {
+    this.setState({isSignedIn: this.auth.isSignedIn.get()})
+  }
+
+  onSignIn = () => {
+    this.auth.signIn();
+  }
+
+  onSignOut = () => {
+    this.auth.signOut();
+  }
+
+  renderAuthButton() {
+    switch(this.state.isSignedIn) {
+      case true:
+        return (
+          <button onClick={this.onSignOut} className="ui red google button">
+            <i className="google icon"></i>Sign Out
+          </button>
+        );
+      case false:
+        return (
+          <button onClick={this.onSignIn} className="ui blue google button">
+            <i className="google icon"></i>Sign In
+          </button>
+        );
+      default:
+        return null; 
+    }
+  }
   render() {
-    return <div>Google Auth</div>;
+    return <div>
+      {this.renderAuthButton()}
+    </div>;
   }
 }
 
