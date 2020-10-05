9a143c9ba3cfc672479304a670d63c4673d3c27f
kryptondv <kryptondevpl@gmail.com>
2020-10-05 11:06:55 +0200

    add action creators for CRUD operations

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,13 @@
 import streams from '../apis/streams';
-import { SIGN_IN, SIGN_OUT } from './types';
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  GET_STREAMS,
+  GET_STREAM,
+  EDIT_STREAM,
+  DELETE_STREAM,
+} from './types';
 
 export const signIn = userId => {
   return {
@@ -15,5 +23,26 @@ export const signOut = () => {
 };
 
 export const createStream = formValues => async dispatch => {
-  streams.post('/streams', formValues);
+  const response = await streams.post('/streams', formValues);
+  dispatch({ type: CREATE_STREAM, payload: response.data });
+};
+
+export const getStreams = () => async dispatch => {
+  const response = await streams.get('/streams');
+  dispatch({ type: GET_STREAMS, payload: response.data });
+};
+
+export const getStream = id => async dispatch => {
+  const response = await streams.get(`/streams/${id}`);
+  dispatch({ type: GET_STREAM, payload: response.data });
+};
+
+export const editStream = (id, formValues) => async dispatch => {
+  const response = await streams.put(`/streams/${id}`, formValues);
+  dispatch({ type: EDIT_STREAM, payload: response.data });
+};
+
+export const deleteStream = id => async dispatch => {
+  await streams.put(`/streams/${id}`);
+  dispatch({ type: DELETE_STREAM, payload: id });
 };
