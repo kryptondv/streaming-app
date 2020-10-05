7c8ce9dccf1ef14bbad6c6571739754922549a19
kryptondv <kryptondevpl@gmail.com>
2020-10-05 10:24:57 +0200

    add creating a stream functionality

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 
 import App from './components/App';
 import rootReducer from './reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(rootReducer, composeEnhancers(
-    applyMiddleware()
+    applyMiddleware(thunk)
   ));
 
 ReactDOM.render(
