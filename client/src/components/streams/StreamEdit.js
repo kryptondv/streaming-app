8920597ac83a61f151490a7db3d13334b423448d
kryptondv <kryptondevpl@gmail.com>
2020-10-08 13:06:32 +0200

    add StreamEdit functionality

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,31 +1,39 @@
-import React, { Component } from 'react';
+import _ from 'lodash';
+import React from 'react';
 import { connect } from 'react-redux';
-import { getStream } from '../../actions';
+import { getStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
 
-class StreamEdit extends Component {
+class StreamEdit extends React.Component {
   componentDidMount() {
-    this.props.getStream(this.props.match.params.id)
+    this.props.getStream(this.props.match.params.id);
   }
+
+  onSubmit = formValues => {
+    this.props.editStream(this.props.match.params.id, formValues);
+  };
 
   render() {
     if (!this.props.stream) {
-      return <div>Loading...</div>
+      return <div>Loading...</div>;
     }
+
     return (
-    <div>
-      {this.props.stream.title}
-    </div>
-  );
+      <div>
+        <h3>Edit a Stream</h3>
+        <StreamForm
+          initialValues={_.pick(this.props.stream, 'title', 'description')}
+          onSubmit={this.onSubmit}
+        />
+      </div>
+    );
   }
 }
-
-
-
 
 const mapStateToProps = (state, ownProps) => {
-  return {
-    stream: state.streams[ownProps.match.params.id]
-  }
-}
+  return { stream: state.streams[ownProps.match.params.id] };
+};
 
-export default connect(mapStateToProps, { getStream })(StreamEdit);
+export default connect(mapStateToProps, { getStream, editStream })(
+  StreamEdit
+);
